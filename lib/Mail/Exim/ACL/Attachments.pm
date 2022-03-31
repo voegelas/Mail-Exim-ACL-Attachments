4142e525d10dde49701d7d445ef317137da12813
Andreas Vögele <andreas@andreasvoegele.com>
2022-03-31 06:58:11 +0200

    Bump version

--- lib/Mail/Exim/ACL/Attachments.pm
+++ lib/Mail/Exim/ACL/Attachments.pm
@@ -6,7 +6,7 @@ use 5.016;
 use warnings;
 use utf8;
 
-our $VERSION = 1.001;
+our $VERSION = 1.002;
 
 use Exporter qw(import);
 use IO::Uncompress::Unzip;
@@ -281,7 +281,7 @@ Mail::Exim::ACL::Attachments - Reject email attachments
 
 =head1 VERSION
 
-version 1.001
+version 1.002
 
 =head1 SYNOPSIS
 
